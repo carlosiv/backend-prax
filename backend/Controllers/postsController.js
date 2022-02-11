bd3f1783a7e7be7cd1681981bd45c37943e0cd28
carlos <cvelasco@skourse.com>
2022-02-11 18:43:38 +0800

    add express-async-handler

--- backend/Controllers/postsController.js
+++ backend/Controllers/postsController.js
@@ -1,23 +1,23 @@
-const getPosts = (req, res) => {
-  res.status(200).json({ posts: "posts" });
-};
+const asyncHandler = require("express-async-handler");
 
-const addPost = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
+  res.status(200).json({ posts: "posts" });
+});
+const addPost = asyncHandler(async (req, res) => {
   if (!req.body.post) {
     res.status(400);
     throw new Error("Please add post");
   }
   res.status(200).json({ message: `${req.body.post} added successfully` });
-};
+});
 
-const editPost = (req, res) => {
+const editPost = asyncHandler(async (req, res) => {
   res.status(200).json({ posts: `update post ${req.params.id}` });
-};
+});
 
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({ posts: `delete post ${req.params.id}` });
-};
-
+});
 module.exports = {
   getPosts,
   addPost,
